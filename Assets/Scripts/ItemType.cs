e0546e200218a178bc4b0b60f0d8e1f55d5beaaa
Daniel Riissanen <daniel.riissanen@gmail.com>
2021-05-12 19:57:22 +0300

    feat: Add salt and sugar items

--- Assets/Scripts/ItemType.cs
+++ Assets/Scripts/ItemType.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 public enum ItemType
 {
     Grain,
+    Salt,
+    Sugar
 }
 
 public class Items
 {
     public static Dictionary<ItemType, string> names = new Dictionary<ItemType, string>()
     {
-        { ItemType.Grain, "grain" }
+        { ItemType.Grain, "grain" },
+        { ItemType.Salt, "salt" },
+        { ItemType.Sugar, "sugar" },
     };
 }
