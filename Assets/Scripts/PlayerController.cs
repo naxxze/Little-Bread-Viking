3890aedec3c53c9f33cfd109e5b6a96d4df6f8e0
naxxze <31542136+naxxze@users.noreply.github.com>
2021-05-12 20:47:19 +0300

    Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,28 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 	public float jumpstr;
+	private float timeout;
 	private Rigidbody2D rb;
+	private HashSet<Collider2D> CurrColl = new HashSet<Collider2D>();
+	
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Ground") CurrColl.Add(col.collider);
+	}
+	
+	void OnCollisionExit2D(Collision2D col) {
+		if(col.gameObject.tag == "Ground") CurrColl.Remove(col.collider);
+	}
 	
 	void Awake()
     {
 		rb = gameObject.GetComponent<Rigidbody2D>();
+		rb.freezeRotation = true;
 	}
 	
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeout = 0.0f;
     }
 
     // Update is called once per frame
@@ -25,7 +36,15 @@ public class PlayerController : MonoBehaviour
         float hori = Input.GetAxis("Horizontal"); 
 		float vert = Input.GetAxis("Vertical"); 
 		
-		rb.AddForce(transform.up*jumpstr*vert);
-		rb.AddForce(transform.right*speed*hori);
+		timeout += Time.deltaTime;
+		
+		if(CurrColl.Count > 0 && vert > 0 && timeout > 0){
+			rb.AddForce(transform.up*jumpstr, ForceMode2D.Impulse);
+			timeout = -0.2f;
+		}
+		if(Mathf.Abs(hori) > 0.01) rb.AddForce(transform.right*(speed - Mathf.Abs(rb.velocity.x))*hori);
+		else if(CurrColl.Count > 0) rb.AddForce(-transform.right*speed*Mathf.Sign(rb.velocity.x)*hori*0.2f);
+
     }
 }
+
